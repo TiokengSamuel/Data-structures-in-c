88166feba2dcb8988a2c4a472fbdfa65cf040b16
TiokengSamuel <samueledward40@gmail.com>
2020-11-20 17:00:19 +0100

    swap

--- swap and sort.c
+++ swap and sort.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
-const int n = 5;
-void swap(int A[], int i, int j) {
-    int temp;
-    temp = A[i];
-    A[i] = A[j];
-    A[j] = temp;
-}
-void findmax (int A[]) {
+int n = 5;
+int main() {
     int max = 0;
     int i, j;
+    int A[];
     for (i = 0; i<n; i++) {
         for (j = i+1; j<n; j++) {
             if (A[i] < A[j]){
@@ -17,4 +12,14 @@ void findmax (int A[]) {
             }
         }
     }
+    return 0;
 }
+
+void swap(int A[], int i, int j) {
+    int temp;
+    temp = A[i];
+    A[i] = A[j];
+    A[j] = temp;
+}
+
+
